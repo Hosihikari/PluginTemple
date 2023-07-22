c29714be0504182efcf4ae5c3c769fd451133f1d
LazuliKao <lazulikao@nbt.edu.cn>
2023-07-22 21:42:43 +0800

    init

---
+++ src/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using Loader;
+
+[assembly: EntryPoint<PluginUnitTest.UnitTest>]
+
+namespace PluginUnitTest;
+
+public class UnitTest : IPlugin
+{
+    public void Initialize(Plugin plugin)
+    {
+        Console.WriteLine(nameof(UnitTest) + " Loaded.");
+        Minecraft.LevelTick.PostTick(() =>
+        {
+            Console.WriteLine(nameof(UnitTest) + " PostTick Success.");
+        });
+    }
+}
